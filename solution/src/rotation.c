7f73f913d7f03159e77b849add00b4757216d20f
ne-umnepatop <113514164+ne-umnepatop@users.noreply.github.com>
2024-01-19 01:14:52 +0300

    ирп

--- solution/src/rotation.c
+++ solution/src/rotation.c
@@ -1,4 +1,19 @@
 #include "rotation.h"
 
-// TODO: Реализация функций для поворота изображения
-struct image rotate_90(struct image const source){};  
+struct image rotate(const struct image* source) {
+    // Реализация поворота изображения на 90 градусов
+    struct image rotated;
+    rotated.width = source->height;
+    rotated.height = source->width;
+    rotated.data = (struct pixel*)malloc(rotated.width * rotated.height * sizeof(struct pixel));
+    rotated.padding = (uint8_t*)calloc(1, sizeof(uint8_t)); // изошёл из бессмысленности инициализации не нулями
+
+    for (uint64_t y = 0; y < rotated.height; ++y) {
+        for (uint64_t x = 0; x < rotated.width; ++x) {
+            rotated.data[y * rotated.width + x] = source->data[(source->height - x - 1) * source->width + y];
+        }
+    }
+
+    return rotated;
+}
+
