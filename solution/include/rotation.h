bade1bcda8902e7c752d7914715eee6d71f57cb6
ne-umnepatop <113514164+ne-umnepatop@users.noreply.github.com>
2024-01-19 01:19:02 +0300

    ou[

--- solution/include/rotation.h
+++ solution/include/rotation.h
@@ -1,6 +1,8 @@
 #ifndef ROTATION_H
 #define ROTATION_H
 #include "image.h"
+#include <stdint.h>
+#include <stddef.h>
 
 // Функция для поворота изображения на 90 градусов
 struct image rotate_90(struct image const source);
--- solution/src/rotation.c
+++ solution/src/rotation.c
@@ -1,18 +1,26 @@
 #include "rotation.h"
 
-struct image rotate(const struct image* source) {
+struct image* rotate(const struct image* source) {
     // Реализация поворота изображения на 90 градусов
-    struct image rotated;
-    rotated.width = source->height;
-    rotated.height = source->width;
-    rotated.data = (struct pixel*)malloc(rotated.width * rotated.height * sizeof(struct pixel));
-    rotated.padding = (uint8_t*)calloc(1, sizeof(uint8_t)); // изошёл из бессмысленности инициализации не нулями
+    struct image* rotated = (struct image*)malloc(sizeof(struct image));
+    rotated->width = source->height;
+    rotated->height = source->width;
+    rotated->data = (struct pixel*)malloc(rotated->width * rotated->height * sizeof(struct pixel));
+    rotated->padding = (uint8_t*)calloc(1, sizeof(uint8_t));
 
-    for (uint64_t y = 0; y < rotated.height; ++y) {
-        for (uint64_t x = 0; x < rotated.width; ++x) {
-            rotated.data[y * rotated.width + x] = source->data[(source->height - x - 1) * source->width + y];
+    if (rotated->data == NULL || rotated->padding == NULL) {
+        // Handle memory allocation failure
+        // ...
+    }
+
+    for (size_t y = 0; y < rotated->height; ++y) {
+        for (size_t x = 0; x < rotated->width; ++x) {
+            rotated->data[y * rotated->width + x] = source->data[(source->height - x - 1) * source->width + y];
         }
     }
+
+    free(source->data);
+    free(source->padding);
 
     return rotated;
 }
