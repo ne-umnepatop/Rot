2823b9c32d2d90e1578ab57b44b2cde219e81e03
ne-umnepatop <113514164+ne-umnepatop@users.noreply.github.com>
2024-01-19 12:38:59 +0300

    main не дописан

--- solution/src/main1.c
+++ solution/src/main1.c
@@ -5,46 +5,39 @@
 #include "bmp.h"
 #include "rotation.h"
 
-const int ROTATION_ANGLE = 90;
-
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: ./image-transformer <source-image> <transformed-image>\n");
-        return 1;
-    }
-
-    // Validate input file paths
-    if (!is_valid_file_path(argv[1])) {
-        fprintf(stderr, "Invalid source image file path.\n");
-        return 1;
-    }
-    if (!is_valid_file_path(argv[2])) {
-        fprintf(stderr, "Invalid transformed image file path.\n");
+        printf("Usage: ./image-transformer <source-image> <transformed-image>\n");
         return 1;
     }
 
     // Load the source image
-    Image *image = load_bmp(argv[1]);
-    if (image == NULL) {
-        fprintf(stderr, "Failed to load the source image.\n");
-        return 1;
+    const char* filename = argv[1];
+    const char* mode = "r";
+    enum file_status status;
+    FILE* file = open_file(filename, mode, &status);
+    if (file != NULL) {
+        printf("File opened successfully\n");
+    } else {
+        printf("Failed to open file\n");
+    }
+    // Перевожу файл bmp во внутренний image 
+    struct image img;
+    enum read_status status = from_bmp(file, &img);
+    if (status==READ_OK){
+
     }
 
-    // Rotate the image counterclockwise using the constant rotation angle
-    Image *rotatedImage = rotate_image(image, ROTATION_ANGLE);
 
-    // Check if rotation operation failed
-    if (rotatedImage == NULL) {
-        printf("Failed to rotate the image.\n");
-        free_image(image);
-        return 1;
-    }
 
-    // Save the transformed image
+
+
+
+
+
+        // Save the transformed image
     if (!save_bmp(rotatedImage, argv[2])) {
-        fprintf(stderr, "Failed to save the transformed image.\n");
-        free_image(image);
-        free_image(rotatedImage);
+        printf("Failed to save the transformed image.\n");
         return 1;
     }
 
